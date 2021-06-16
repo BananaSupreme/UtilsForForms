ff7346a5c369a477e5119bc0e1910cbbbfec9fb6
Celery <Supremus>
2021-06-16 20:27:48 +0300

    Workaround heb language issue 1866 added

---
+++ Xamarin.Forms.Utils.Android/Helpers/LanguageWorkAround1866.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+using Java.Util;
+
+namespace Xamarin.Forms.Utils.Droid.Helpers
+{
+	public class LanguageWorkAround1866
+	{
+		public static void FixLanguageError()
+		{
+			// Mono mishandles Hebrew, Indonesian, and Arabic identifiers and sets the app to the invariant culture.
+			Locale locale = Locale.GetDefault( Locale.Category.Display );
+			CultureInfo culture = locale.ISO3Language switch
+			{
+				"heb" => CultureInfo.GetCultureInfo( "he" ),
+				"ind" => CultureInfo.GetCultureInfo( "id" ),
+				"ara" => CultureInfo.GetCultureInfo( "ar" ),
+				_ => CultureInfo.CurrentUICulture
+			};
+
+			CultureInfo.CurrentCulture = culture;
+			CultureInfo.CurrentUICulture = culture;
+		}
+	}
+}
